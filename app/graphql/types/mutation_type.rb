5484e57bb82d1a49e687212762d5ec2fdc602d11
cookietriestoprogram <ma.chinaortiz@gmail.com>
2026-03-22 01:55:25 +0800

    add: CUD mutations and define ticket typ

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,5 +5,6 @@ module Types
     field :create_ticket, mutation: Mutations::CreateTicket
     field :update_ticket, mutation: Mutations::UpdateTicket
     field :delete_ticket, mutation: Mutations::DeleteTicket
+    field :login, mutation: Mutations::Login
   end
 end
