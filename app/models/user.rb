c03903b01dec3ded2839bb5f387e31c49df6c030
cookietriestoprogram <ma.chinaortiz@gmail.com>
2026-03-22 01:06:34 +0800

    fix: reference with ticket

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     enum role: { user: 0, admin: 1 }
     has_secure_password
+
+    has_many :tickets
+    has_many :assigned_tickets, class_name: "Ticket", foreign_key: "assigned_to_id"
 end
